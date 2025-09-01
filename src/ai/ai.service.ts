7f276cd453e966bde4f63417aa9c4e52b4a241de
kvinash <kapilyadav93@gmail.com>
2025-09-01 21:49:06 +0530

    update-open-ruoter-config

--- src/ai/ai.service.ts
+++ src/ai/ai.service.ts
@@ -8,17 +8,26 @@ export class AiService {
 
   constructor() {
     this.llm = new ChatOpenAI({
-      model: "gpt-3.5-turbo",
+      configuration: {
+        baseURL: "https://openrouter.ai/api/v1",
+        defaultHeaders: {
+          "HTTP-Referer": "https://localhost:3000", // Your site URL
+          "X-Title": "Langchain MultiDB NestJS", // Your site name
+        },
+      },
+      modelName: "openai/gpt-3.5-turbo", // Prefix with 'openai/' when using OpenRouter
       temperature: 0,
-      apiKey: process.env.OPENAI_API_KEY,
+      openAIApiKey: process.env.OPENROUTER_API_KEY,
     });
   }
 
-  async generateSQL(query: string): Promise<string> {
+  async generateSQL(query: string): Promise<any> {
     try {
+    
       const prompt = `Generate optimized PostgreSQL SQL for: ${query}. Return ONLY SQL.`;
       const res = await this.llm.invoke(prompt);
-      return res.toString();
+      console.log("Generated SQL:", JSON.stringify(res, null, 2));
+      return res
     } catch (err) {
       console.error("Error generating SQL:", err);
       return "";
@@ -31,7 +40,7 @@ export class AiService {
       based on this request: "${query}". Return ONLY valid JSON array.`;
 
       const res = await this.llm.invoke(prompt);
-      return JSON.parse(res.toString());
+      return ;
     } catch (err) {
       console.error("Error generating MongoDB pipeline:", err);
       return [];
